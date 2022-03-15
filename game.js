4fb46ae963de18c42faa01a72b65997e14cc5d00
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-03-14 22:57:55 -0700

    added styling

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
 //this is game.js
+
+// ----------- CREATE CHARACTERS
+
+const character = document.querySelector('#character');
+character.src= "https://im2.ezgif.com/tmp/ezgif-2-ab210f8700.gif";
+
+const block = document.querySelector("#block")
+block.src="https://im2.ezgif.com/tmp/ezgif-2-143abd5a86.png"
+
+// ------------- CREATE ACTION FUNCTIONS
+
+function jump () {
+    character.classList.toggle("animate");
+}
+
+character.addEventListener( "click", () => jump() )
